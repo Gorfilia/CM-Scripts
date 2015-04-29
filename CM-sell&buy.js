523979bfbf3a6a8f0d559f2c0f65a04be9032637
Gorfilia <gorold@hotmail.fr>
2015-04-29 23:50:10 +0200

    Update CM-sell&buy.js
    Refactoring for good practice

--- CM-sell&buy.js
+++ CM-sell&buy.js
@@ -1,149 +1,144 @@
 // ==UserScript==
 // @name       Sell & buy CM
 // @namespace  croqueScript
-// @version    0.1
+// @version    0.2
 // @description  script for send or buy item in croquemonster
 // @include     http://www.croquemonster.com/exchange*
 // @copyright  2012+, You
 // ==/UserScript==
 
-/**
-* Function to buy object
-* @param idObject id of object
-* @param numberBuy number of object will be buy
-* */
-function buy(idObject, numberBuy) {
-	for(var i = 0; i < numberBuy; i++) {
-			Exchange.increment(idObject);
-	}
-}
-
-/**
-* Function to create html element
-* @return HTMLElement
-* */
-function element(name) {
-	return document.createElement(name);
-}
-
-/**
-* Function to create html's select
-* @param options's array
-* @return select (the HTMLElement)
-* */
-function selectElement(optionsList) {
-	var select = element("select");
-	
-	for(var i in optionsList) {
-		if(!isNaN(i) &&
-		 (optionsList[i] != 'undefined' && optionsList[i] != undefined)) {
-			var option = element("option");
-			option.value = i;
-			option.text = optionsList[i];
-			select.add(option);
+(function() {
+	var DOMManipulation = {
+		/**
+		 * Function to create html's select
+		 * @param options's array
+		 * @return select (the HTMLElement)
+		 * */
+		selectElement: function(optionsList) {
+			var select = document.createElement("select");
+			
+			for(var i in optionsList) {
+				if(!isNaN(i) && (optionsList[i] != 'undefined' && optionsList[i] != undefined)) {
+					var option = document.createElement("option");
+					option.value = i;
+					option.text = optionsList[i];
+					select.add(option);
+				}
+			}
+			return select;
+		},
+		/**
+		 * Function to catch object's label
+		 * */
+		objectLabels: function() {
+			var h1 = document.querySelectorAll('.txt h1'), result = [], index = 0;
+			//FIXME loop is strange
+			for(var i in h1) {
+				result[index] = h1[i].innerHTML;
+				index++;
+			}
+			return result;
+		},
+				
+		/**
+		 * Function to create Frame
+		 * */
+		Frame: function() {
+			this.body = document.createElement('div');
+			this.elements = [];
+			
+			document.body.appendChild(this.body);
+			
+			this.body.style.position = 'fixed';
+			this.body.style.top = '25%';
+			this.body.style.left = '25%';
+			
+			this.body.style.height = '50%';
+			this.body.style.width = '50%';
+			this.body.style.margin = 'auto';
+			
+			this.body.style.backgroundColor = 'black';
+			this.body.style.opacity = '0.9';
+			
+			this.body.style.textAlign = "center";
 		}
-	}
-	return select;
-}
-
-/**
-* Constructor
-**/
-function Frame() {
-	this.body = element('div');
-	this.elements = [];
-
-	document.body.appendChild(this.body);
-
-	this.body.style.position = 'fixed';
-	this.body.style.top = '25%';
-	this.body.style.left = '25%';
-
-	this.body.style.height = '50%';
-	this.body.style.width = '50%';
-	this.body.style.margin = 'auto';
-
-	this.body.style.backgroundColor = 'black';
-	this.body.style.opacity = '0.9';
-
-	this.body.style.textAlign = "center";
-}
-
-Frame.prototype = {
-	add: function(htmlElement) {
+	},
+	
+	Job = function() {
+		var _labels = DOMManipulation.objectLabels();
+		this.labels = _labels;
+		/**
+		 * Function to buy object
+		 * idObject id of object
+		 * numberBuy number of object will be buy
+		 * */
+		this.buy = function(idObject, numberBuy) {
+			for(var i = 0; i < numberBuy; i++) {
+				Exchange.increment(idObject);
+			}
+		}
+	};
+	
+	DOMManipulation.Frame.prototype.add = function(htmlElement) {
 		this.elements.push(htmlElement);
 		this.body.appendChild(htmlElement);
-		
+
 		htmlElement.style.position = 'relative';
 		htmlElement.style.top = '50%';
 		htmlElement.style.margin = 'auto';
-	},
-	removeAll: function() {
+	};
+	DOMManipulation.Frame.prototype.removeAll = function() {
 		while(this.elements.length > 0) {
 			this.elements.pop();
 			this.body.removeChild(this.body.lastChild);
 		}
-	},
-	
-	show: function() {
+	};
+	DOMManipulation.Frame.prototype.show = function() {
 		this.body.style.display = 'inline';
-	},
-	hide: function() {
+	};
+	DOMManipulation.Frame.prototype.hide = function() {
 		this.body.style.display = 'none';
-	}
-};
-
-/**
-* Function to catch object's label
-* */
-function objectLabels() {
-	var h1 = document.querySelectorAll('.txt h1'), result = [], index = 0;
-	for(var i in h1) {
-		result[index] = h1[i].innerHTML;
-		index++;
-	}
-	return result;
-}
-
-function queryToUser(str) {
-	var result = prompt(str);
-	if(result)
-		return result;
-	throw "Annulé";
-}
-
-var title = document.querySelector('.noMarg'), button = element('button');
-title.parentNode.insertBefore(button, title.nextSibling);
-
-button.onclick = function() {
-	try {
-		var listObject = objectLabels(),
-		select = selectElement(listObject),
-		f = new Frame, objectValue;
+	};
+	
+	this.run = function() {
+		var title = document.querySelector('.noMarg'), button = document.createElement('button'),
+		job = new Job,
+		select = DOMManipulation.selectElement(job.labels),
+		f = new DOMManipulation.Frame;
 		
-		select.onclick = function(e) {
-			objectValue = select.selectedIndex;
-			f.removeAll();
+		function selectedBuy() {
 			f.hide();
-			var num = queryToUser('Quantité de ' + listObject[objectValue] + ' à acheter : '),
-				result = buy(objectValue, num);
-			var qte = document.querySelector('#my' + objectValue + ' .qte'),
-				result = false;
+			var value = select.selectedIndex,
+			num = prompt(['Quantité de ', job.labels[value], ' à acheter : '].join('')),
+			result = false;
 			
-			if(qte)
-				result = confirm('Acheter ' + qte.innerHTML + ' de ' + listObject[objectValue] + ' ?');
-			if(result)
+			//increment api croquemonster
+			job.buy(value, num);
+			
+			//get quantity to be buy
+			var qte = document.querySelector(['#my', value,' .qte'].join(''));
+			if(qte) {
+				result = confirm(['Acheter ', qte.innerHTML, ' de ', job.labels[value], ' ?'].join(''));
+			}
+			if(result) {
 				Exchange.validate(document.querySelector('.list .actionButton'));
-		}
-		f.add(select);
-		f.show();
-	}catch(err) {
-		console.log(err);
-		alert(err);
-	}
-};
+			}
+		};
+		
+		if(window.location.href != "http://www.croquemonster.com/exchange?v=sell" )
+			button.innerHTML='Buy';
+		else
+			button.innerHTML='Sell';
 
-if(window.location.href != "http://www.croquemonster.com/exchange?v=sell" )
-	button.innerHTML='Buy';
-else
-	button.innerHTML='Sell';
+		/* Add button to DOM */
+		title.parentNode.insertBefore(button, title.nextSibling);
+		
+		f.add(select);
+		f.hide();
+		//TODO manage by button
+		select.addEventListener('click', selectedBuy);
+		button.addEventListener('click', function() { f.show(); });
+	}
+	
+	this.run();
+})();
